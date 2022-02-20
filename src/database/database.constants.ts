2cf72e8d3478942c23985323f2cd27560f4f68df
raghu <h.raghu7234@gmail.com>
2022-02-20 17:39:50 +0530

    nestjs

---
+++ src/database/database.constants.ts
@@ -0,0 +1,4 @@
+export const DATABASE_CONNECTION ='mongodb+srv://suma:1234@cluster0.3bcn5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+export const POST_MODEL = 'POST_MODEL';
+export const USER_MODEL = 'USER_MODEL';
+export const COMMENT_MODEL = 'COMMENT_MODEL';
